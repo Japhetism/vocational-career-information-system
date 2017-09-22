feab13854e9130e6b20bd252e7b46ff50d64a6cd
Japhetism <babatundeojo30@gmail.com>
2017-09-22 11:23:02 +0100

    updates

---
+++ app/Mail/SendMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
+
+class SendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build(Request $request)
+    {
+        return $this->view('mail.contactMail',['description'=>$request->description])
+                ->to($request->email)
+                ->subject('RE: '.$request->subject)
+                ->from('admin@vcis.com', 'Vocational Career Information System');
+    }
+}
